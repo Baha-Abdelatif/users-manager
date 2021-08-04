903ac87a963503e55bafcb3cc09232363d32bd00
Baha-Abdelatif <baha.abdelatif@gmail.com>
2021-08-04 12:38:30 +0200

    Create an user by using Strapi register route

--- client/script.js
+++ client/script.js
@@ -1,10 +1,28 @@
 const register_form = document.forms.register_form;
+const apiUrl = "http://localhost:1337";
 
 register_form.addEventListener('submit', register);
 
 function register(e) {
   e.preventDefault();
-  const email = register_form.email.value;
-  const password = register_form.password.value;
+  const { username, email, password } = register_form;
+  // console.log(username.value, email.value, password.value)
+  const payload = {
+    username: username.value,
+    email: email.value,
+    password: password.value
+  }
+  fetch(`${apiUrl}/auth/local/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(payload)
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+    .catch(err => console.error(err))
 
 }
